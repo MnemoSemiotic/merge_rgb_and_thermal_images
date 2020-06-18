c626f0b21cbf7836f7782eed5b0028454be229ff
Andrew Roberts <clownfragment@gmail.com>
2020-06-18 17:44:43 -0600

    replace lightness with therm

--- merging_utilities/merging.py
+++ merging_utilities/merging.py
@@ -21,3 +21,28 @@ def avg_layers_with_thermal(rgb, thermal):
     rgbArray[..., 2] = avg_mat(B, avg_therm)
 
     return rgbArray
+
+
+def repl_light_with_therm(rgb, thermal, illuminant='D65'):
+    '''
+    chooses from these illuminants: ['A', 'D50', 'D55', 'D65', 'D75', 'E']
+    '''
+
+    # split RGB vals, note: drop of alpha layer
+    img_lab = rgb2lab(rgb, illuminant=illuminant)
+    avg_therm = avg_image(thermal)
+
+    L,A,B = split_layers_to_3(img_lab)
+
+    # set range to fit in LAB L range (0-100)
+    avg_therm_scaled = np.interp(avg_therm, (avg_therm.min(), avg_therm.max()), (0, 100))
+
+    # create empty array, populate for new image
+    newLAB = np.zeros((rgb.shape[0],rgb.shape[1],3))
+    newLAB[:,:,0] = avg_therm_scaled
+    newLAB[:,:,1] = A
+    newLAB[:,:,2] = B
+
+    newRGB = lab2rgb(newLAB)
+
+    return img_as_ubyte(newRGB)
